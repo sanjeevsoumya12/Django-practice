bfc3aa4caf148e73f8878bb5217c27da4be1839c
sanjeevsoumya12 <soumyasanjeev99@gmail.com>
2022-03-06 02:31:59 +0530

    reverse the setting of the secret keys

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",'django-insecure-v*16dtj!x4c8&7b+@5z2^*$r+#+vfksk(*z!&_%*wx3kbh8ep*')
 print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1" #1 == True 
